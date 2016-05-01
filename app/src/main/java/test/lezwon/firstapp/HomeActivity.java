48db1a738b19e909706588a682af1f2529fb9715
lezwon <lezwon@gmail.com>
2016-05-02 02:01:27 +0530

    added drawer menu

--- app/src/main/java/test/lezwon/firstapp/HomeActivity.java
+++ app/src/main/java/test/lezwon/firstapp/HomeActivity.java
@@ -2,9 +2,14 @@ package test.lezwon.firstapp;
 
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +19,24 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        String[] menuArray = getResources().getStringArray(R.array.drawer_array);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openDrawer,
+                R.string.closeDrawer
+        );
+
+        listView.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_item, menuArray));
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
     }
 
     @Override
