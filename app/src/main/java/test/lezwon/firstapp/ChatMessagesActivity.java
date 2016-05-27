bffad9bb5a6d327fb3d1ac867a34419960a57236
lezwon <lezwon@gmail.com>
2016-05-27 13:00:47 +0530

    emoji popup and close on button click profile image in toolbar send icon in textbox image background for chat

--- app/src/main/java/test/lezwon/firstapp/ChatMessagesActivity.java
+++ app/src/main/java/test/lezwon/firstapp/ChatMessagesActivity.java
@@ -4,16 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import com.rockerhieu.emojicon.EmojiconEditText;
 import com.rockerhieu.emojicon.EmojiconGridFragment;
 import com.rockerhieu.emojicon.EmojiconsFragment;
 import com.rockerhieu.emojicon.emoji.Emojicon;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatMessagesActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener,
         EmojiconGridFragment.OnEmojiconClickedListener,
         EmojiconsFragment.OnEmojiconBackspaceClickedListener  {
+
+    private boolean emojiKeyboardOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +36,22 @@ public class ChatMessagesActivity extends AppCompatActivity implements Toolbar.O
             name = (String) savedInstanceState.getSerializable("EXTRA_CHAT_NAME");
         }
 
+
+
+
         /*material design toolbar*/
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar.setTitle(name);
         setSupportActionBar(toolbar);
+
+        /*Get chat toolbar layout*/
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.chat_toolbar_details,toolbar);
 
         /*Initializes action bar abd back button*/
         ActionBar actionBar = getSupportActionBar();
 
         assert actionBar != null;
-        actionBar.setTitle(name);
 
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -64,7 +76,8 @@ public class ChatMessagesActivity extends AppCompatActivity implements Toolbar.O
 
     @Override
     public void onEmojiconClicked(Emojicon emojicon) {
-
+        EmojiconEditText chatBox = (EmojiconEditText) findViewById(R.id.chat_textbox);
+        chatBox.append(emojicon.getEmoji());
     }
 
     @Override
@@ -76,10 +89,20 @@ public class ChatMessagesActivity extends AppCompatActivity implements Toolbar.O
 
     public void openEmoji(View view){
 
-        View fragmentContainer = findViewById(R.id.emojicons);
+        ViewGroup fragmentContainer = (ViewGroup) findViewById(R.id.emojicons);
         ViewGroup.LayoutParams layoutParams = fragmentContainer.getLayoutParams();
-        layoutParams.height = 250;
-        getSupportFragmentManager().beginTransaction().add(R.id.emojicons,new EmojiconGridFragment()).commit();
+
+        if(emojiKeyboardOpen){
+            layoutParams.height = 0;
+            fragmentContainer.removeAllViews();
+            emojiKeyboardOpen = false;
+        }
+        else{
+            layoutParams.height = 250;
+            getSupportFragmentManager().beginTransaction().add(R.id.emojicons,new EmojiconGridFragment()).commit();
+            emojiKeyboardOpen = true;
+        }
+
     }
 
 
