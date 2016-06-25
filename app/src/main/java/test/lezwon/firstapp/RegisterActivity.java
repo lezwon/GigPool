f1aa4fc4acc34199b482d0d3946095c9f8885a4a
lezwon <lezwon@gmail.com>
2016-06-26 01:07:15 +0530

    phonenumber entering sequence

--- app/src/main/java/test/lezwon/firstapp/RegisterActivity.java
+++ app/src/main/java/test/lezwon/firstapp/RegisterActivity.java
@@ -3,23 +3,71 @@ package test.lezwon.firstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import java.util.ArrayList;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayList<EditText> phoneEditTextArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
+
+        phoneEditTextArrayList = new ArrayList<>();
+        phoneEditTextArrayList.add((EditText) findViewById(R.id.text_phone_num_1));
+        phoneEditTextArrayList.add((EditText) findViewById(R.id.text_phone_num_2));
+        phoneEditTextArrayList.add((EditText) findViewById(R.id.text_phone_num_3));
+
+        for (int i = 0; i< phoneEditTextArrayList.size(); i++) {
+            phoneEditTextArrayList.get(i).addTextChangedListener(new PhoneNumberTextboxListener(i));
+        }
     }
 
     @OnClick(R.id.btn_verify_number)
     void verifyNumber(View view){
         Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
         startActivity(intent);
+    }
+
+    private class PhoneNumberTextboxListener implements TextWatcher {
+        int currentTextBoxPos;
+
+        PhoneNumberTextboxListener(int i) {
+            currentTextBoxPos = i;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(editable.length() == 3 && currentTextBoxPos != 2){
+                phoneEditTextArrayList.get(currentTextBoxPos +1).requestFocus();
+                return;
+            }
+
+            //todo backspace deletes from previous edittext
+            if(editable.length() == 0 && currentTextBoxPos != 0){
+                phoneEditTextArrayList.get(currentTextBoxPos -1).requestFocus();
+            }
+
+        }
     }
 }
 
